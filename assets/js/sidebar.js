b0168caa73ee4ae43c89fed801b7e7acb99e922c
Andi <andi-apeunit@users.noreply.github.com>
2018-07-09 13:58:35 +0200

    update files with changes from uxp

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,20 +1,64 @@
 'use strict';
 (function ($) {
 
-	var innerNavLinks = $('.navigation .navigation-inner a')
+	var lsKey = 'se-op';
 
-	$('.navigation > li > a').click(function (e) {
-		$(this).parent().toggleClass('open');
+	var innerNavLinks = $('.navigation .navigation-inner a');
+
+	var classOpen = 'open';
+	var classCurrent = 'current';
+
+	for (var i = 0; i < innerNavLinks.length; i++) {
+		var el = innerNavLinks[i];
+		if ($(el).hasClass('active')) {
+			$(el).closest('.navigation > li').addClass(classOpen);
+			$(el).closest('ul').prev().addClass(classCurrent);
+		}
+	}
+
+	var sectionsOpen = loadOpenSections(lsKey);
+
+	$.each(sectionsOpen, function (i, e) {
+		var el = $('[data-id="' + e + '"]');
+		if (el) {
+			el.addClass(classOpen);
+		}
 	});
 
-	innerNavLinks.click(function (e) {
+	$('.navigation > li > a').click(function (ev) {
+		$(this).parent().toggleClass(classOpen);
+		var newId = $(this).parent().data('id') + '';
 
-		innerNavLinks.removeClass('active');
-		$(this).addClass('active');
-
-		$('.navigation a.current').removeClass('current');
-		$(this).parent().parent().prev().addClass('current');
-		
+		if ($(this).parent().hasClass(classOpen) && sectionsOpen.indexOf(newId) < 0) {
+			sectionsOpen.push(newId);
+		} else if (!$(this).parent().hasClass(classOpen) && sectionsOpen.indexOf(newId) >= 0) {
+			sectionsOpen = sectionsOpen.filter(function (el) {
+				return el != newId;
+			});
+		}
+		window.localStorage.setItem(lsKey, sectionsOpen.join(', '));
 	});
+
+	function loadOpenSections(key) {
+		var storedSections = window.localStorage.getItem(key);
+
+		if (storedSections && document.referrer !== '') {
+			return storedSections.split(', ');
+		}
+
+		var arr = [];
+		var currentlyOpened = $('.navigation li.open');
+		if (currentlyOpened.length) {
+			$.each(currentlyOpened, function () {
+				var id = currentlyOpened.data('id') + '';
+				if (id && arr.indexOf(id) < 0) {
+					arr.push(id);
+				}
+			});
+		}
+
+		window.localStorage.setItem(key, arr.join(', '));
+		return arr;
+	}
 
 })(Zepto)
